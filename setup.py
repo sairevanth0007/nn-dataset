b1ce63135584d3fd9d5b3109aa7bfb123e93cb25
ABrain-One <112828851+ABrain-One@users.noreply.github.com>
2025-02-08 22:28:03 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ def read_readme():
 
 setup(
     name="NN-Dataset",
-    version="1.0.3",
+    version="1.0.4",
     description="Neural Network Dataset",
     long_description=read_readme(),
     long_description_content_type="text/markdown",
