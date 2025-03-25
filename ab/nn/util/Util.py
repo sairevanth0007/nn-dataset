8922c00fd0ac5213a425c92d08dc7e60d9b09e95
goodarzi <arash.orca99@gmail.com>
2025-03-25 06:38:04 +0100

    added onnx saving via API

--- ab/nn/util/Util.py
+++ ab/nn/util/Util.py
@@ -115,7 +115,7 @@ def save_model_as_onnx(model, model_name, dummy_input):
         if isinstance(layer, (torch.nn.AdaptiveAvgPool2d, torch.nn.AdaptiveMaxPool2d)):
             if layer.output_size not in [(1, 1), 1, None]:
                 hasAdaptivePoolingLayer = True
-    onnx_file_path = f"../Onnx_exports/{model_name}.onnx"
+    onnx_file_path = f"./Onnx_exports/{model_name}.onnx"
 
     if hasAdaptivePoolingLayer:
         torch.onnx.export(
